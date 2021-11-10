51e6d46ab8007480a6ccefc21d162ebbcff9dae2
ivanerios <93769784+ivanerios@users.noreply.github.com>
2021-11-10 11:04:20 +0100

    Add files via upload

---
+++ ob-spring-ej1/src/main/java/com/example/Saludo.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Saludo {
+    public Saludo(){
+        System.out.println("Ejecutando saludo");
+    }
+    public String imprimirSaludo(){
+        return "Hola holita vecinito!";
+    }
+}
